e83f3c2f7ba082d990f04a7294d352d4a7eac778
ajaya timilsina <ajayatimilsina1@gmail.com>
2025-04-26 00:40:53 +0900

    person web

---
+++ ajaya_timilsina_website/readme.md
@@ -0,0 +1,10 @@
+
+Clone the Repo
+Build The Image
+This reads your Dockerfile and creates a Docker image.
+
+docker-compose build
+Run the container
+The -d flag runs containers in the background (detached mode).
+
+docker-compose up -d
--- ajaya_timilsina_website/settings.py
+++ ajaya_timilsina_website/settings.py
@@ -11,11 +11,11 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'homeapp' / 'static',
+]
+
